c30aa26636f2bf3c71fa2adab9d8fdafd5af5082
Kevin Shi <im@shikev.in>
2015-03-26 14:39:14 -0400

    getting the attribute info of app and passing it back to background.js

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,24 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  alert('injected');
-  chrome.tabs.executeScript(null, {file: "content_script.js"});
+var recording = false;
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+  sendResponse({farewell: message });
 });
+
+//  chrome.runtime.onMessage.addListener(
+//    function(request, sender, sendResponse) {
+//      alert('message received');
+//    }
+//    sendResponse( {here: request.attributes });
+//  );
+chrome.browserAction.onClicked.addListener(function(tab) {
+  if (recording) {
+    alert('stoped recording');
+    recording = false;
+  } else {
+    alert('recording...');
+    chrome.tabs.executeScript(null, {file: "jquery.js"}, function() {
+       chrome.tabs.executeScript(null, {file: "content_script.js" })
+    });
+    recording = true;
+  }
+});
+
--- extension/content_script.js
+++ extension/content_script.js
@@ -1,7 +1,32 @@
-var button = document.getElementById("bt_login");
-console.log(button);
-console.log('here');
-button.addEventListener("click", function(event) {
-  alert(event);
-  alert("hello.");
+document.addEventListener("mousedown", function(event) {
+  var currentElement = event.target;
+  var attributes = {};
+  if (currentElement == null) {
+    return;
+  }
+  do {
+    for (var i = 0, atts = currentElement.attributes, n = atts.length; i < n; i++) {
+      attribute = atts[i];
+      if (attribute.nodeName == "class") {
+        attributes['class'] = currentElement.classList;
+      } else if (attribute.nodeName == "style") {
+        //skip
+      } else {
+        attributes[attribute.nodeName] = attribute.nodeValue;
+      }
+    }
+    currentElement = currentElement.parentElement;
+  } while($.isEmptyObject(attributes));
+
+  chrome.runtime.sendMessage({ element: attributes }, function (response) {
+    console.log(response.farewell.element);
+  });
 });
+
+//var button = document.getElementById("bt_login");
+//console.log(button);
+//console.log('here');
+//button.addEventListener("click", function(event) {
+//  alert(event);
+//  alert("hello.");
+//});

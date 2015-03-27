63fb2242b639bb657f5b7239e1ec24f4ab06b6be
Kevin Shi <im@shikev.in>
2015-03-27 03:18:38 +0000

    colors in action on the server

--- prof_server/routes/chrome.js
+++ prof_server/routes/chrome.js
@@ -6,10 +6,37 @@ var saveNumber = 0;
 router.post('/', function (req, res) {
   var saveLocation = './screenshots/' + saveNumber + '.html';
   var saveText = req.body.page;
-  console.log(req.body.event);
-  console.log(req.body.element);
   saveNumber++;
+  var attributes = JSON.parse(req.body.element);
 
+  var jquerySelector = "$(\"";
+
+  function addClassAttr (classes) {
+    for (style in classes) {
+      if (!(isNaN(style)) && classes.hasOwnProperty(style)) {
+        jquerySelector +="[class~='"+classes[style]+"']";
+      }
+    }
+  }
+    
+  for (attr in attributes) {
+    if (attributes.hasOwnProperty(attr)) {
+      if (attr == 'class') {
+        addClassAttr(attributes[attr]);
+      } else {
+        jquerySelector += "["+attr+"='"+attributes[attr]+"']";
+      }
+    }
+  }
+  jquerySelector+="\")";
+
+  var injectedJavascript = "<script src=http://code.jquery.com/jquery-1.11.2.min.js type='text/javascript'></script>" +
+  "<script type='text/javascript'>" +
+    jquerySelector + ".css({'color':'red', 'border': '5px solid red'});"+ "</script>";
+
+  console.log(injectedJavascript);
+
+  saveText += injectedJavascript;
   fs.writeFile(saveLocation, saveText, function(err) {
     if (err) {
       return console.log(err);

5c25aee6935235054cb779f788574838f6cdc53b
Kevin Shi <im@shikev.in>
2015-03-27 12:41:18 -0400

    added sessions to save files to

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,6 @@
 var recording = false;
+var hostName = "http://54.175.195.224:3000";
+var current_session = "";
 //  chrome.runtime.onMessage.addListener(
 //    function(request, sender, sendResponse) {
 //      alert('message received');
@@ -6,10 +8,11 @@ var recording = false;
 //    sendResponse( {here: request.attributes });
 //  );
 var pageReceiver =  function(message, sender, sendResponse) {
-  $.post("http://54.175.195.224:3000/chrome", { 
+  $.post(hostName + "/chrome", { 
         'page' : message.page,
         'event' : "mousedown", 
-        'element' : JSON.stringify(message.element) 
+        'element' : JSON.stringify(message.element), 
+        'session' : current_session
       }, function (data) {
     console.log(data);
     sendResponse(data);
@@ -18,11 +21,16 @@ var pageReceiver =  function(message, sender, sendResponse) {
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   if (recording) {
-    alert('stoped recording');
+    alert('stoped recording.\nSee it here: ' +
+      hostName + "/screenshots/" + current_session + "/0.html");
     recording = false;
     chrome.runtime.onMessage.removeListener(pageReceiver); 
   } else {
     alert('recording...');
+    $.get(hostName + "/session", function(data) {
+      current_session = data;
+      console.log(current_session);
+    });
     chrome.runtime.onMessage.addListener(pageReceiver);
     chrome.tabs.executeScript(null, {file: "jquery.js"}, function() {
        chrome.tabs.executeScript(null, {file: "content_script.js" })
